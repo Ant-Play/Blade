91c183edbec6a73711bbc35c5ce1a5ab2c6cf399
JiaChen He <m13109919299@163.com>
2024-02-29 09:55:42 +0800

    add ALS

--- Source/Blade/Blade.Build.cs
+++ Source/Blade/Blade.Build.cs
@@ -8,7 +8,7 @@ public class Blade : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "MotionWarping", "ThirdPersonCamera"});
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "MotionWarping", "ThirdPersonCamera", "ALS"});
 		PublicIncludePaths.AddRange(new string[] {"ThirdPersonCamera/Public"});
 	}
 }
